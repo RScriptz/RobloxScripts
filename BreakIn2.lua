60860e869106639d1dda57fe14d74d516fe3766f
RScriptz <135446272+RScriptz@users.noreply.github.com>
2023-09-27 19:54:30 +0300

    Update BreakIn2.lua

--- BreakIn2.lua
+++ BreakIn2.lua
@@ -1,6 +1,34 @@
 -- Place Check
 if game.PlaceId ~= 13864667823 then
 	if game.PlaceId == 14775231477 or game.PlaceId == 13864661000 then
+		
+	-- Free Gamepasses (LOBBY)
+	local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/RScriptz/RobloxScripts/main/OrionLibKeybind.lua')))()
+	local Window = OrionLib:MakeWindow({
+		Name = "Breaking Blitz",
+		HidePremium = false,
+		SaveConfig = true,
+		ConfigFolder = "OrionTest",
+		IntroText = "Loading Script..."
+	})
+	local Tab = Window:MakeTab({
+		Name = "Free Gamepasses",
+		Icon = "rbxassetid://4483345998",
+		PremiumOnly = false
+	})
+	Tab:AddButton({
+		Name = "Free Hacker Role",
+		Callback = function()
+			game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole:FireServer("Phone", true, false)
+		end
+	})
+	Tab:AddButton({
+		Name = "Free Nerd Kid Role",
+		Callback = function()
+			game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole:FireServer("Book", true, false)
+		end
+	})
+	OrionLib:Init()
 		for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
 			if v.Name == "Part" and v:FindFirstChild("TouchInterest") then
 				firetouchinterest(v, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0)
